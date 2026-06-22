d395cf41e2bc7a9f840e6122dbc40f892d637dcf
Arun <140481988+arundeveloper2731@users.noreply.github.com>
2026-06-22 14:00:30 +0530

    spring security

---
+++ webdevelopment/login/LoginController.java
@@ -0,0 +1,19 @@
+package com.example.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String homePage(){
+        return "home";
+    }
+    
+}
